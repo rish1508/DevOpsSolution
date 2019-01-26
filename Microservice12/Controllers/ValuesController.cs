fb91c0efa804a8c34d2fa26466b9e9e3e0dbc2d0
Rishabh Gupta <rishabhgupta1508@gmail.com>
2019-01-26 19:55:27 +0100

    git

--- Microservice12/Controllers/ValuesController.cs
+++ Microservice12/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Microservice12.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Rishabh", "Gupta" };
         }
 
         // GET api/values/5
